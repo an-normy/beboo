eea0f12a32d438183e39bfec9f168efef0646659
an-normy <112652463+an-normy@users.noreply.github.com>
2022-11-01 17:28:40 -0300

    Add files via upload

---
+++ Console.js
@@ -0,0 +1,22 @@
+var text = alert("here goes some functions that console have:")
+var text = alert("open console just press Ctrl,Shift and I and here you goes!")
+var FN = "Bob"
+var LN = " Marley"
+
+var AL = FN.concat(LN)
+console.log(AL)
+console.log(AL.toUpperCase())
+console.log(AL.toLowerCase())
+console.log(AL.length)
+
+var text = alert("one more thing to show for you")
+ var one = prompt("Type the first number here")
+ var two = prompt("Type the second number here")
+
+ var result1 = Math.max(one,two)
+ console.log("max: " + result1)
+
+ var result2 = Math.min(two,one)
+ console.log("min: " + result2)
+
+
